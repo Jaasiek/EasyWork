98f8f52e8780f2b0690e8d45e3962815584fa6fa
Jasiek <u119_jangaw_waw@technischools.com>
2024-05-20 13:24:00 +0200

    hours converting

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 lokalizacja_csv = input(
@@ -14,9 +15,23 @@ program = df_clean["PROGRAM"].values.tolist()
 nazwa_plików = df_clean["NAZWA_PLIKU"].values.tolist()
 
 
-print(program)
+# print(godziny_emisji)
 
-print(
-    program[nazwa_plików.index("0604_World_News_Flash_2300_ZDP-3345782J")],
-    godziny_emisji[nazwa_plików.index("0604_World_News_Flash_2300_ZDP-3345782J")],
-)
+# print(
+#     program[nazwa_plików.index("0604_World_News_Flash_2300_ZDP-3345782J")],
+#     godziny_emisji[nazwa_plików.index("0604_World_News_Flash_2300_ZDP-3345782J")],
+# )
+
+
+def convert_to_int(godzina):
+    if godzina.endswith("+"):
+        godzina = godzina[:-1]
+    time_obj = datetime.strptime(godzina, "%H:%M:%S.%f")
+    int_representation = time_obj.hour * 10000 + time_obj.minute * 100 + time_obj.second
+    return int_representation
+
+
+# Convert each time string to HHMMSS format as an integer
+int_list = [convert_to_int(godzina) for godzina in godziny_emisji]
+
+print(int_list)
