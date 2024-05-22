365872f1b3bccfb776c2447d794d0bef15368d47
Jasiek <u119_jangaw_waw@technischools.com>
2024-05-22 14:37:19 +0200

    folders

--- easy_work_final.py
+++ easy_work_final.py
@@ -1,20 +1,26 @@
+# Imports
 import pandas as pd
-from functions import convert_to_int, remove_elements
-from early_news import early_news_fcpxml
-from early_talks import early_talks_fcpxml
-from early_trends import early_trends_fcpxml
-from today_news import today_news_fcpxml
-from today_talks import today_talks_fcpxml
-from today_trends import today_trends_fcpxml
-from tonight_news import tonight_news_fcpxml
-from tonight_talks import tonight_talks_fcpxml
-from tonight_trends import tonight_trends_fcpxml
+from functions.helping_functions import convert_to_int, remove_elements
+from functions.early_news import early_news_fcpxml
+from functions.early_talks import early_talks_fcpxml
+from functions.early_trends import early_trends_fcpxml
+from functions.today_news import today_news_fcpxml
+from functions.today_talks import today_talks_fcpxml
+from functions.today_trends import today_trends_fcpxml
+from functions.tonight_news import tonight_news_fcpxml
+from functions.tonight_talks import tonight_talks_fcpxml
+from functions.tonight_trends import tonight_trends_fcpxml
 
-
-csv_path = input("Podaj lokalizację pliku CSV (przeciągnąć plik do terminala) : ")
-
+# Inputs
+fcpx_library = input(
+    "\nPodaj ściężkę do swojej biblioteki FCPX (przeciągnij do terminala): "
+)
+fcpx_library = fcpx_library.strip("'")
+csv_path = input("Podaj lokalizację pliku CSV (przeciągnąć plik do terminala): ")
 csv_path = csv_path.strip("'")
+event_name = input("Podaj nazwę eventu, do którego chcesz przypisać dane projekty: ")
 
+# CSV read and modification
 df = pd.read_csv(f"{csv_path}", sep=";")
 df_clean = df.dropna()
 emmision_hours = remove_elements(df["GODZINA_EMISJI"].values.tolist())
@@ -25,3 +31,5 @@ hours_int = [convert_to_int(hour) for hour in emmision_hours]
 print(emmision_hours, len(emmision_hours))
 print(program_name, len(program_name))
 print(file_names, len(file_names))
+
+# Creating files
--- functions/early_news.py
+++ functions/early_news.py
@@ -1,7 +1,8 @@
 import xml.etree.ElementTree as ET
 
+
 def early_news_fcpxml(file, library_location, event_name, name):
-    
+
     fcpxml_root_early_news = ET.Element("fcpxml", version="1.10")
     resources_early_news = ET.SubElement(fcpxml_root_early_news, "resources")
     format_early_news = ET.SubElement(
@@ -15,245 +16,253 @@ def early_news_fcpxml(file, library_location, event_name, name):
         colorSpace="1-1-1(Rec. 709)",
     )
     asset_early_news = ET.SubElement(
-    resources_early_news,
-    "asset",
-    id="r2",
-    name="WIPE_NEWS_EARLY_OUT",
-    uid="F29BF49EE15C5916E6BA2226630E19D5",
-    start="0s",
-    duration="9700/2500s",
-    hasVideo="1",
-    format="r1",
-    hasAudio="1",
-    videoSources="1",
-    audioSources="1",
-    audioChannels="2",
-    audioRate="48000",
-)
+        resources_early_news,
+        "asset",
+        id="r2",
+        name="WIPE_NEWS_EARLY_OUT",
+        uid="F29BF49EE15C5916E6BA2226630E19D5",
+        start="0s",
+        duration="9700/2500s",
+        hasVideo="1",
+        format="r1",
+        hasAudio="1",
+        videoSources="1",
+        audioSources="1",
+        audioChannels="2",
+        audioRate="48000",
+    )
     media_rep_out_early_news = ET.SubElement(
-    asset_early_news,
-    "media-rep",
-    kind="original-media",
-    sig="F29BF49EE15C5916E6BA2226630E19D5",
-    src="file:///Volumes/TVPWORLD/OPRAWA%202024/OPRAWA%20NEWS/WIPE%20EARLY/WIPE_NEWS_EARLY_OUT.mov",
-)
+        asset_early_news,
+        "media-rep",
+        kind="original-media",
+        sig="F29BF49EE15C5916E6BA2226630E19D5",
+        src="file:///Volumes/TVPWORLD/OPRAWA%202024/OPRAWA%20NEWS/WIPE%20EARLY/WIPE_NEWS_EARLY_OUT.mov",
+    )
     bookmark_out_early_news = ET.SubElement(media_rep_out_early_news, "bookmark")
     bookmark_out_early_news.text = "Ym9va/ADAAAAAAQQMAAAAENN1iUibcqKdgoAMUiHRZ3CAtVGbgLatxfE4j+kvDFwBAMAAAQAAAADAwAAABgAKAcAAAABAQAAVm9sdW1lcwAIAAAAAQEAAFRWUFdPUkxECwAAAAEBAABPUFJBV0EgMjAyNAALAAAAAQEAAE9QUkFXQSBORVdTAAoAAAABAQAAV0lQRSBFQVJMWQAAFgAAAAEBAABXSVBFX05FV1NfRUFSTFlfSU4ubW92AAAYAAAAAQYAABAAAAAgAAAAMAAAAEQAAABYAAAAbAAAAAgAAAAEAwAAMmMAAAAAAAAAAAAAAQoAABgAAAABBgAArAAAALwAAAC8AAAAvAAAALwAAAC8AAAAGAAAAAECAAABAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAAAAAAAAQUAAAQAAAADAwAABwAAAAQAAAADAwAABAAAABkAAAABCQAAZmlsZTovLy9Wb2x1bWVzL1RWUFdPUkxELwAAAAgAAAAEAwAAAAAAAABkAAAYAAAAAQIAAAAAAAAAAAAA7xMAAAEAAAAAAAAAAAAAABEAAAABAQAAL1ZvbHVtZXMvVFZQV09STEQAAAAbAAAAAQkAAG5mczovLzEwLjE2LjIwLjIyMy9UVlBXT1JMRAAIAAAAAQkAAGZpbGU6Ly8vDAAAAAEBAABNYWNpbnRvc2ggSEQIAAAABAMAAACgIGh0AAAACAAAAAAEAABBxVGOmIAAACQAAAABAQAANzJFMTZENzQtRTgzNi00NjQwLTk2NkQtOTFFOEQwMzdENjFDGAAAAAECAACBAAAAAQAAAO8TAAABAAAAAAAAAAAAAAABAAAAAQEAAC8AAABgAAAA/v///wDwAAAAAAAABwAAAAIgAABIAgAAAAAAAAUgAAC4AQAAAAAAABAgAADIAQAAAAAAABEgAAD8AQAAAAAAABIgAADcAQAAAAAAABMgAADsAQAAAAAAACAgAAAoAgAAAAAAAAQAAAADAwAAAPAAAAQAAAADAwAAAAAAAAQAAAADAwAAAQAAABwAAAABBgAAvAIAAMgCAADUAgAAyAIAAMgCAADIAgAAyAIAALQAAAD+////AQAAAFQCAAAOAAAABBAAAIwAAAAAAAAABRAAAMQAAAAAAAAAEBAAAOQAAAAAAAAAVBAAAAwBAAAAAAAAVRAAABgBAAAAAAAAVhAAAAQBAAAAAAAAACAAAOACAAAAAAAAAiAAAHgBAAAAAAAABSAAACQBAAAAAAAAECAAACAAAAAAAAAAEiAAAEgBAAAAAAAAICAAAFgBAAAAAAAAUCAAAJQBAAAAAAAAENAAAAQAAAAAAAAA"
     metadata_out_early_news = ET.SubElement(asset_early_news, "metadata")
     md1_out_early_news = ET.SubElement(
-    metadata_out_early_news,
-    "md",
-    key="com.apple.proapps.studio.rawToLogConversion",
-    value="0",
-)
+        metadata_out_early_news,
+        "md",
+        key="com.apple.proapps.studio.rawToLogConversion",
+        value="0",
+    )
     md2_out_early_news = ET.SubElement(
-    metadata_out_early_news,
-    "md",
-    key="com.apple.proapps.spotlight.kMDItemProfileName",
-    value="HD (1-1-1)",
-)
+        metadata_out_early_news,
+        "md",
+        key="com.apple.proapps.spotlight.kMDItemProfileName",
+        value="HD (1-1-1)",
+    )
     md3_out_early_news = ET.SubElement(
-    metadata_out_early_news, "md", key="com.apple.proapps.studio.cameraISO", value="0"
-)
+        metadata_out_early_news,
+        "md",
+        key="com.apple.proapps.studio.cameraISO",
+        value="0",
+    )
     md4_out_early_news = ET.SubElement(
-    metadata_out_early_news,
-    "md",
-    key="com.apple.proapps.studio.cameraColorTemperature",
-    value="0",
-)
+        metadata_out_early_news,
+        "md",
+        key="com.apple.proapps.studio.cameraColorTemperature",
+        value="0",
+    )
     md5_out_early_news = ET.SubElement(
-    metadata_out_early_news, "md", key="com.apple.proapps.spotlight.kMDItemCodecs"
-)
+        metadata_out_early_news, "md", key="com.apple.proapps.spotlight.kMDItemCodecs"
+    )
     array_md5_out_early_news = ET.SubElement(md5_out_early_news, "array")
     string1_out_early_news = ET.SubElement(array_md5_out_early_news, "string")
     string1_out_early_news.text = "Apple ProRes 4444"
     string2_out_early_news = ET.SubElement(array_md5_out_early_news, "string")
     string2_out_early_news.text = "Linear PCM"
     md6_out_early_news = ET.SubElement(
-    asset_early_news,
-    "md",
-    key="com.apple.proapps.mio.ingestDate",
-    value="2024-03-26 16:37:46 +0100",
-)
+        asset_early_news,
+        "md",
+        key="com.apple.proapps.mio.ingestDate",
+        value="2024-03-26 16:37:46 +0100",
+    )
     asset_r3_early_news = ET.SubElement(
-    resources_early_news,
-    "asset",
-    id="r3",
-    name="WIPE_NEWS_EARLY_IN",
-    uid="87E4F87453AD395DF0CE4CB50714E112",
-    start="0s",
-    duration="7600/2500s",
-    hasVideo="1",
-    format="r1",
-    hasAudio="1",
-    videoSources="1",
-    audioSources="1",
-    audioChannels="2",
-    audioRate="48000",
-)
+        resources_early_news,
+        "asset",
+        id="r3",
+        name="WIPE_NEWS_EARLY_IN",
+        uid="87E4F87453AD395DF0CE4CB50714E112",
+        start="0s",
+        duration="7600/2500s",
+        hasVideo="1",
+        format="r1",
+        hasAudio="1",
+        videoSources="1",
+        audioSources="1",
+        audioChannels="2",
+        audioRate="48000",
+    )
     media_rep_in_early_news = ET.SubElement(
-    asset_r3_early_news,
-    "media-rep",
-    kind="original-media",
-    sig="87E4F87453AD395DF0CE4CB50714E112",
-    src="file:///Volumes/TVPWORLD/OPRAWA%202024/OPRAWA%20NEWS/WIPE%20EARLY/WIPE_NEWS_EARLY_IN.mov",
-)
+        asset_r3_early_news,
+        "media-rep",
+        kind="original-media",
+        sig="87E4F87453AD395DF0CE4CB50714E112",
+        src="file:///Volumes/TVPWORLD/OPRAWA%202024/OPRAWA%20NEWS/WIPE%20EARLY/WIPE_NEWS_EARLY_IN.mov",
+    )
     bookmark_in_early_news = ET.SubElement(media_rep_in_early_news, "bookmark")
     bookmark_in_early_news.text = "Ym9va/ADAAAAAAQQMAAAAENN1iUibcqKdgoAMUiHRZ3CAtVGbgLatxfE4j+kvDFwBAMAAAQAAAADAwAAABgAKAcAAAABAQAAVm9sdW1lcwAIAAAAAQEAAFRWUFdPUkxECwAAAAEBAABPUFJBV0EgMjAyNAALAAAAAQEAAE9QUkFXQSBORVdTAAoAAAABAQAAV0lQRSBFQVJMWQAAFgAAAAEBAABXSVBFX05FV1NfRUFSTFlfSU4ubW92AAAYAAAAAQYAABAAAAAgAAAAMAAAAEQAAABYAAAAbAAAAAgAAAAEAwAAMmMAAAAAAAAAAAAAAQoAABgAAAABBgAArAAAALwAAAC8AAAAvAAAALwAAAC8AAAAGAAAAAECAAABAAAAAAAAAA8AAAAAAAAAAAAAAAAAAAAAAAAAAQUAAAQAAAADAwAABwAAAAQAAAADAwAABAAAABkAAAABCQAAZmlsZTovLy9Wb2x1bWVzL1RWUFdPUkxELwAAAAgAAAAEAwAAAAAAAABkAAAYAAAAAQIAAAAAAAAAAAAA7xMAAAEAAAAAAAAAAAAAABEAAAABAQAAL1ZvbHVtZXMvVFZQV09STEQAAAAbAAAAAQkAAG5mczovLzEwLjE2LjIwLjIyMy9UVlBXT1JMRAAIAAAAAQkAAGZpbGU6Ly8vDAAAAAEBAABNYWNpbnRvc2ggSEQIAAAABAMAAACgIGh0AAAACAAAAAAEAABBxVGOmIAAACQAAAABAQAANzJFMTZENzQtRTgzNi00NjQwLTk2NkQtOTFFOEQwMzdENjFDGAAAAAECAACBAAAAAQAAAO8TAAABAAAAAAAAAAAAAAABAAAAAQEAAC8AAABgAAAA/v///wDwAAAAAAAABwAAAAIgAABIAgAAAAAAAAUgAAC4AQAAAAAAABAgAADIAQAAAAAAABEgAAD8AQAAAAAAABIgAADcAQAAAAAAABMgAADsAQAAAAAAACAgAAAoAgAAAAAAAAQAAAADAwAAAPAAAAQAAAADAwAAAAAAAAQAAAADAwAAAQAAABwAAAABBgAAvAIAAMgCAADUAgAAyAIAAMgCAADIAgAAyAIAALQAAAD+////AQAAAFQCAAAOAAAABBAAAIwAAAAAAAAABRAAAMQAAAAAAAAAEBAAAOQAAAAAAAAAVBAAAAwBAAAAAAAAVRAAABgBAAAAAAAAVhAAAAQBAAAAAAAAACAAAOACAAAAAAAAAiAAAHgBAAAAAAAABSAAACQBAAAAAAAAECAAACAAAAAAAAAAEiAAAEgBAAAAAAAAICAAAFgBAAAAAAAAUCAAAJQBAAAAAAAAENAAAAQAAAAAAAAA"
     metadata_in_early_news = ET.SubElement(asset_r3_early_news, "metadata")
     md1_in_early_news = ET.SubElement(
-    metadata_in_early_news,
-    "md",
-    key="com.apple.proapps.studio.rawToLogConversion",
-    value="0",
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.studio.rawToLogConversion",
+        value="0",
+    )
     md2_in_early_news = ET.SubElement(
-    metadata_in_early_news,
-    "md",
-    key="com.apple.proapps.spotlight.kMDItemProfileName",
-    value="HD (1-1-1)",
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.spotlight.kMDItemProfileName",
+        value="HD (1-1-1)",
+    )
     md3_in_early_news = ET.SubElement(
-    metadata_in_early_news, "md", key="com.apple.proapps.studio.cameraISO", value="0"
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.studio.cameraISO",
+        value="0",
+    )
     md4_in_early_news = ET.SubElement(
-    metadata_in_early_news,
-    "md",
-    key="com.apple.proapps.studio.cameraColorTemperature",
-    value="0",
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.studio.cameraColorTemperature",
+        value="0",
+    )
     md5_in_early_news = ET.SubElement(
-    metadata_in_early_news, "md", key="com.apple.proapps.studio.cameraColorTemperature"
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.studio.cameraColorTemperature",
+    )
     array_md5_in_early_news = ET.SubElement(md5_in_early_news, "array")
     string1_in_early_news = ET.SubElement(array_md5_in_early_news, "string")
     string1_in_early_news.text = "Apple ProRes 4444"
     string2_in_early_news = ET.SubElement(md5_in_early_news, "string")
     string2_in_early_news.text = "Linear PCM"
     md6_in_early_news = ET.SubElement(
-    metadata_in_early_news,
-    "md",
-    key="com.apple.proapps.mio.ingestDate",
-    value="2024-03-26 16:37:46 + 0100",
-)
+        metadata_in_early_news,
+        "md",
+        key="com.apple.proapps.mio.ingestDate",
+        value="2024-03-26 16:37:46 + 0100",
+    )
 
     library_early_news = ET.SubElement(
-    fcpxml_root_early_news, "library", location=f"{library_location}"
-)
+        fcpxml_root_early_news, "library", location=f"{library_location}"
+    )
     event_early_news = ET.SubElement(
-    library_early_news,
-    "event",
-    name=f"{event_name}",
-    uid="02D0CB09-3878-4C40-A592-3925110BCD4D",
-)
+        library_early_news,
+        "event",
+        name=f"{event_name}",
+        uid="02D0CB09-3878-4C40-A592-3925110BCD4D",
+    )
     project_early_news = ET.SubElement(
-    event_early_news,
-    "project",
-    name=f"{name}",
-    uid="FDFBBBBD-D964-4E17-ADED-09E90A76AB73",
-    modDate="2024-04-06 20:14:03 +0200",
-)
+        event_early_news,
+        "project",
+        name=f"{name}",
+        uid="FDFBBBBD-D964-4E17-ADED-09E90A76AB73",
+        modDate="2024-04-06 20:14:03 +0200",
+    )
     sequence_early_news = ET.SubElement(
-    project_early_news,
-    "sequence",
-    fomrat="r1",
-    duration="17300/2500s",
-    tcStart="0s",
-    tcFormat="NDF",
-    audioLayout="stereo",
-    audioRate="48k",
-)
+        project_early_news,
+        "sequence",
+        fomrat="r1",
+        duration="17300/2500s",
+        tcStart="0s",
+        tcFormat="NDF",
+        audioLayout="stereo",
+        audioRate="48k",
+    )
     spine_early_news = ET.SubElement(sequence_early_news, "spine")
     gap1_early_news = ET.SubElement(
-    spine_early_news,
-    "gap",
-    name="Gap",
-    offset="0s",
-    start="3600s",
-    duration="9700/2500s",
-)
+        spine_early_news,
+        "gap",
+        name="Gap",
+        offset="0s",
+        start="3600s",
+        duration="9700/2500s",
+    )
     asset_clip1_early_news = ET.SubElement(
-    gap1_early_news,
-    "asset-clip",
-    ref="r2",
-    lane="1",
-    offset="3600s",
-    name="WIPE_NEWS_EARLY_OUT",
-    duration="9700/2500s",
-    tcFormat="NDF",
-    audioRole="dialogue",
-)
+        gap1_early_news,
+        "asset-clip",
+        ref="r2",
+        lane="1",
+        offset="3600s",
+        name="WIPE_NEWS_EARLY_OUT",
+        duration="9700/2500s",
+        tcFormat="NDF",
+        audioRole="dialogue",
+    )
     keyword_asset_clip1_early_news = ET.SubElement(
-    asset_clip1_early_news,
-    "keyword",
-    start="0s",
-    duration="9700/2500s",
-    value="wipe early",
-)
+        asset_clip1_early_news,
+        "keyword",
+        start="0s",
+        duration="9700/2500s",
+        value="wipe early",
+    )
     marker_asset_clip_early_news = ET.SubElement(
-    asset_clip1_early_news,
-    "marker",
-    start="12/5s",
-    duration="100/2500s",
-    value="Marker 1",
-)
+        asset_clip1_early_news,
+        "marker",
+        start="12/5s",
+        duration="100/2500s",
+        value="Marker 1",
+    )
     asset_clip2_early_news = ET.SubElement(
-    spine_early_news,
-    "asset-clip",
-    ref="r3",
-    offset="9700/2500s",
-    name="WIPE_NEWS_EARLY_IN",
-    duration="7600/2500s",
-    tcFormat="NDF",
-    audioRole="dialogue",
-)
+        spine_early_news,
+        "asset-clip",
+        ref="r3",
+        offset="9700/2500s",
+        name="WIPE_NEWS_EARLY_IN",
+        duration="7600/2500s",
+        tcFormat="NDF",
+        audioRole="dialogue",
+    )
     keyword_asset_clip2_early_news = ET.SubElement(
-    asset_clip2_early_news,
-    "keyword",
-    start="0s",
-    duration="7600/2500s",
-    value="wipe early",
-)
+        asset_clip2_early_news,
+        "keyword",
+        start="0s",
+        duration="7600/2500s",
+        value="wipe early",
+    )
     marker_asset_clip2_early_news = ET.SubElement(
-    asset_clip2_early_news,
-    "marker",
-    start="11/25s",
-    duration="100/2500s",
-    value="Marker 1",
-)
+        asset_clip2_early_news,
+        "marker",
+        start="11/25s",
+        duration="100/2500s",
+        value="Marker 1",
+    )
     smart_collection1_early_news = ET.SubElement(
-    library_early_news, "smart-collection", name="Projects", match="all"
-)
+        library_early_news, "smart-collection", name="Projects", match="all"
+    )
     match_clip_smart_collection1_early_news = ET.SubElement(
-    smart_collection1_early_news, "match-clip", rule="is", type="project"
-)
+        smart_collection1_early_news, "match-clip", rule="is", type="project"
+    )
     smart_collection2_early_news = ET.SubElement(
-    library_early_news, "smart-collection", name="All Video", match="any"
-)
+        library_early_news, "smart-collection", name="All Video", match="any"
+    )
     match_media1_smart_collection2_early_news = ET.SubElement(
-    smart_collection2_early_news, "match-media", rule="is", type="videoOnly"
-)
+        smart_collection2_early_news, "match-media", rule="is", type="videoOnly"
+    )
     match_media2_smart_collection2_early_news = ET.SubElement(
-    smart_collection2_early_news, "match-media", rule="is", type="videoWithAudio"
-)
+        smart_collection2_early_news, "match-media", rule="is", type="videoWithAudio"
+    )
     smart_collection3_early_news = ET.SubElement(
-    library_early_news, "smart-collection", name="Audio Only", match="all"
-)
+        library_early_news, "smart-collection", name="Audio Only", match="all"
+    )
     match_media1_smart_collection3_early_news = ET.SubElement(
-    smart_collection3_early_news, "match-media", rule="is", type="audioOnly"
-)
+        smart_collection3_early_news, "match-media", rule="is", type="audioOnly"
+    )
     smart_collection4_early_news = ET.SubElement(
-    library_early_news, "smart-collection", name="Stills", match="all"
-)
+        library_early_news, "smart-collection", name="Stills", match="all"
+    )
     match_media1_smart_collection_4_early_news = ET.SubElement(
-    smart_collection4_early_news, "match-media", rule="is", type="stills"
-)
+        smart_collection4_early_news, "match-media", rule="is", type="stills"
+    )
     smart_collection5_early_news = ET.SubElement(
-    library_early_news, "smart-collection", name="Favourites", match="all"
-)
+        library_early_news, "smart-collection", name="Favourites", match="all"
+    )
     match_ratings1_smart_collection4_early_news = ET.SubElement(
-    smart_collection5_early_news, "match-ratings", value="favourites"
-)
+        smart_collection5_early_news, "match-ratings", value="favourites"
+    )
 
     header = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE fcpxml>\n'
 
@@ -264,4 +273,3 @@ def early_news_fcpxml(file, library_location, event_name, name):
     fcpxml_early_news = header + fcpxml_string_early_news
 
     file.write(fcpxml_early_news)
-
