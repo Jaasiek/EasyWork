6525156d6cafbec76cc895ba12c07b910e3d70e4
Jaasiek <145573055+Jaasiek@users.noreply.github.com>
2024-05-31 00:22:17 +0200

    Update today_trends.py

--- functions/today_trends.py
+++ functions/today_trends.py
@@ -137,7 +137,7 @@ def today_trends_fcpxml(file, library_location, event_name, path):
     array_md5_in_today_trends = ET.SubElement(md5_in_today_trends, "array")
     string1_in_today_trends = ET.SubElement(array_md5_in_today_trends, "string")
     string1_in_today_trends.text = "Apple ProRes 4444"
-    string2_in_today_trends = ET.SubElement(md5_in_today_trends, "string")
+    string2_in_today_trends = ET.SubElement(array_md5_in_today_trends, "string")
     string2_in_today_trends.text = "Linear PCM"
     md6_in_today_trends = ET.SubElement(
         metadata_in_today_trends,
